885fa50531bc204633257af3da5cfc5a725d1366
Lapham <90162642+BraxelGN@users.noreply.github.com>
2023-12-06 20:44:21 -0300

    Update empanadas_delete.js

--- js/empanadas_delete.js
+++ js/empanadas_delete.js
@@ -1,4 +1,4 @@
-const URL = "http://localhost:5000";
+const URL = "https://braxelgn.pythonanywhere.com/";
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
